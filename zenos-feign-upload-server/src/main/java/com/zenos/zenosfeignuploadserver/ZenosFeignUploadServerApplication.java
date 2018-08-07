5b7d32f27f85f104dd0f7af4ccf65c6ebc09fae5
zhangsu <zhangsu@lvmama.com>
2018-08-07 11:51:12 +0800

    feign 文件

---
+++ zenos-feign-upload-server/src/main/java/com/zenos/zenosfeignuploadserver/ZenosFeignUploadServerApplication.java
@@ -0,0 +1,32 @@
+package com.zenos.zenosfeignuploadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZenosFeignUploadServerApplication {
+
+
+    @RestController
+    public class UploadController {
+
+        @PostMapping(value = "/uploadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+        public String handleFileUpload(@RequestPart(value = "file") MultipartFile file) {
+            return file.getName();
+        }
+
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenosFeignUploadServerApplication.class, args);
+	}
+}
