443f3e376a3527202fac33b3e34a5a15347aa38b
zhangsu <zhangsu@lvmama.com>
2018-08-10 18:54:01 +0800

    熔断

---
+++ zenos-turbine/src/main/java/com/zenos/zenosturbine/ZenosTurbineApplication.java
@@ -0,0 +1,20 @@
+package com.zenos.zenosturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableTurbine
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZenosTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenosTurbineApplication.class, args);
+	}
+}
